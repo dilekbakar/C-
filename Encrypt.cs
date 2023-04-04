0373c59c7d33745e34a06a777e8e12b9ac9e3e51
Dilek BAKAR <108486801+dilekbakar@users.noreply.github.com>
2023-04-04 11:35:14 +0300

    commit
    new commit

---
+++ Encrypt.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CSharp
+{
+    public partial class Encrypt : System.Web.UI.Page
+    {
+      
+        string hash = "sifrelenecekveri";
+
+        protected void btnEncrypt_Click(object sender, EventArgs e)
+        {
+
+            byte[] data = UTF8Encoding.UTF8.GetBytes(txtSifre.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    txtEncrypt.Text = Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+
+        }
+}
