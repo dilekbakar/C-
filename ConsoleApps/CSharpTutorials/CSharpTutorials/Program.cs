466048f3533e8e55ed6b3fcf7cc4282dcc706666
Dilek BAKAR <108486801+dilekbakar@users.noreply.github.com>
2023-04-11 02:22:49 +0300

    commit
    html parser tutorial

--- ConsoleApps/CSharpTutorials/CSharpTutorials/Program.cs
+++ ConsoleApps/CSharpTutorials/CSharpTutorials/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HtmlAgilityPack;
+
+HtmlWeb web= new HtmlWeb();
+HtmlDocument doc = web.Load("https://example.com/");
+
+var title = doc.DocumentNode.SelectNodes("//div/h1").First().InnerText;
+var desc = doc.DocumentNode.SelectNodes("//div/p").First().InnerText;
+
+Console.WriteLine(title);
+Console.WriteLine(desc);
